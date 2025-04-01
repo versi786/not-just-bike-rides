02e7aade33b685b8726bd4b434eced6f2e356e5a
Aasif Versi <versi786@gmail.com>
2025-03-31 21:18:34 -0400

    use themed

--- src/components/ride.jsx
+++ src/components/ride.jsx
@@ -2,12 +2,13 @@ import * as React from "react"
 import { InstagramEmbed } from 'react-social-media-embed';
 import { jsx, Heading } from "theme-ui"
 import { Paragraph, Text } from 'theme-ui'
+import { Themed } from '@theme-ui/mdx'
 
 function Ride({  instagramUrl, routeUrl, className}) {
     // TODO: why does a paragraph/link in jsx not match style of markdown paragraph?
   return (
   <>
-    <h3>Route: <a href={routeUrl}>{routeUrl}</a></h3>
+    <Themed.p>Route: <Themed.a href={routeUrl}>{routeUrl}</Themed.a></Themed.p>
 
     <div style={{ display: 'flex', justifyContent: 'center' }}>
         <InstagramEmbed url={instagramUrl} caption width={500} />
