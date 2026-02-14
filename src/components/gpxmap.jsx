00675b93228452d07e3abbdbeb31f41a36ce2590
Aasif Versi <versi786@gmail.com>
2026-02-14 17:40:58 -0500

    fix maps

--- src/components/gpxmap.jsx
+++ src/components/gpxmap.jsx
@@ -151,7 +151,7 @@ const GpxMap = ({ src }) => {
       setIsDownloading(false);
     }
   };
-  const attribution = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
   return (
     <div style={{ width: '100%', borderRadius: '12px', overflow: 'hidden', border: '1px solid #eee', background: 'white' }}>
       
@@ -178,7 +178,7 @@ const GpxMap = ({ src }) => {
             wheelPxPerZoomLevel={60} // Adjusts scroll sensitivity for fractional zoom
           >
           <TileLayer 
-            url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png" 
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" 
             crossOrigin="anonymous" 
             attribution={attribution}
           />
