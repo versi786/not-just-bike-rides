160f399d0bcd62c4427c79dafc588e5e4e43a19a
Aasif Versi <versi786@gmail.com>
2026-02-14 12:00:18 -0500

    vibe code a gpx map component

--- src/components/ride.jsx
+++ src/components/ride.jsx
@@ -1,25 +1,36 @@
 import * as React from "react"
 import { InstagramEmbed } from 'react-social-media-embed';
-import { jsx, Heading } from "theme-ui"
-import { Paragraph, Text } from 'theme-ui'
 import { Themed } from '@theme-ui/mdx'
+// Import the GpxMap component we created in the previous steps
+import GpxMap from "./gpxmap" 
 
-function Ride({  instagramUrl, routeUrl, routeGpx,className}) {
+function Ride({ instagramUrl, routeUrl, routeGpx, className }) {
+  const gpxPath = `/gpx/${routeGpx}`;
+
   return (
-  <>
-    <Themed.p>
-        GPX: <Themed.a href={`/gpx/${routeGpx}`}>download</Themed.a>
+    <div className={className}>
+      <Themed.p>
+        GPX: <Themed.a href={gpxPath}>download</Themed.a>
         <br />
-        Route: <Themed.a href={routeUrl}>Strava</Themed.a>
-    </Themed.p>
+        Route: <Themed.a href={routeUrl} target="_blank" rel="noreferrer">Strava</Themed.a>
+      </Themed.p>
 
-    <Themed.p>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <InstagramEmbed url={instagramUrl} caption width={500} />
-      </div>
-    </Themed.p>
+      <Themed.p>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '100%', maxWidth: '500px' }}>
+            <GpxMap src={gpxPath} height="400px" />
+          </div>
+        </div>
+      </Themed.p>
 
-  </>
+      {instagramUrl && (
+        <Themed.p>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <InstagramEmbed url={instagramUrl} caption width={500} />
+          </div>
+        </Themed.p>
+      )}
+    </div>
   )
 }
 
